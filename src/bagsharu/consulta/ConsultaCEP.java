7653f77f4b9842378480376e5a45a50b56706abb
Haru <gabs.haru@gmail.com>
2024-10-04 16:55:55 -0400

    Implementação de consulta do CEP informado.

--- src/bagsharu/consulta/ConsultaCEP.java
+++ src/bagsharu/consulta/ConsultaCEP.java
@@ -1,10 +1,32 @@
 package bagsharu.consulta;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaCEP {
 
-    public  Info Consultar(String cepConsulta) {
+    public Info Consultar(String cepConsulta) {
+
+        var busca = "https://viacep.com.br/ws/" + cepConsulta + "/json/";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(busca))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            System.out.println("CEP Inválido!");
+        }
 
 
-        return ;
+        return new Gson().fromJson(response.body(), Info.class);
     }
 }
