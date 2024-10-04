3fdc50c72fee19f26782719e290a88621c9670a2
Haru <gabs.haru@gmail.com>
2024-10-04 16:56:45 -0400

    Instanciação de classes e impressão das informações.

--- src/bagsharu/principal/Principal.java
+++ src/bagsharu/principal/Principal.java
@@ -1,5 +1,8 @@
 package bagsharu.principal;
 
+import bagsharu.consulta.ConsultaCEP;
+import bagsharu.consulta.Info;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -7,9 +10,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Principal {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args){
 
 
+        /*
         // Recebe a URL correspondente do CEP.
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -20,5 +24,11 @@ public class Principal {
 
         // Verificação da requisição json.
         System.out.println(response.body());
+        */
+
+        ConsultaCEP consultarCEP = new ConsultaCEP();
+
+        Info endereco = consultarCEP.Consultar("01001000");
+        System.out.println(endereco);
     }
 }
