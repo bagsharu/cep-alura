490e14d2b8bcdbd69833c4446255e315833cc74c
Haru <gabs.haru@gmail.com>
2024-10-04 17:41:18 -0400

    Comentários da classe ConsultaCEP

--- src/bagsharu/consulta/ConsultaCEP.java
+++ src/bagsharu/consulta/ConsultaCEP.java
@@ -1,9 +1,6 @@
 package bagsharu.consulta;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -13,17 +10,27 @@ public class ConsultaCEP {
 
     public Info Consultar(String cepConsulta) {
 
+        // Recebe o CEP para consulta e concatena em uma URL.
+
         var busca = "https://viacep.com.br/ws/" + cepConsulta + "/json/";
+
+        // Instancia um objeto "client" e "request"
         HttpClient client = HttpClient.newHttpClient();
+
+        // Realiza o acesso à API viaCEP.
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(busca))
                 .build();
+
+        // Bloco try-catch para lidar com erros de formatação
         try {
+
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
             return new Gson().fromJson(response.body(), Info.class);
 
         } catch (Exception e) {
+            // Caso o CEP tenha sido informado incorretamente, lança a seguinte mensagem:
             throw new RuntimeException("CEP inválido! Não foi possível realizar a operação.");
         }
 
