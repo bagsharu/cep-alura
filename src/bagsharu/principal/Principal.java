a2b4e8204c937cc9805a9bdc353650d411a817ee
Haru <gabs.haru@gmail.com>
2024-10-04 17:36:13 -0400

    Comentários código Principal

--- src/bagsharu/principal/Principal.java
+++ src/bagsharu/principal/Principal.java
@@ -2,16 +2,37 @@ package bagsharu.principal;
 
 import bagsharu.consulta.ConsultaCEP;
 import bagsharu.consulta.Info;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Principal {
     public static void main(String[] args){
+
+        // Instanciação de objetos utilizados.
+        ConsultaCEP consultarCEP = new ConsultaCEP();
+        Scanner leitura = new Scanner(System.in);
+
+        // Entrada de informações pelo usuário.
+        System.out.println("Informe o CEP a ser recebido: ");
+        System.out.println("Atente-se à formação, o CEP NÃO pode conter mais que oito digitos ou caracteres além de números!");
+
+        var buscaCEP = leitura.nextLine();
+
+        // Bloco try-catch para lidar com possíveis erros de entrada
+        try {
+
+            // Instancia objeto "endereco" e consulta o CEP informado posteriormente
+            Info endereco = consultarCEP.Consultar(buscaCEP);
+            System.out.println("As informações foram registradas em " + endereco.cep() + ".json !");
+
+            // Instacia objeto "gerarArquivo" para manipulação e registro de dados em um arquivo.
+            GeradorArquivo gerarArquivo = new GeradorArquivo();
+            gerarArquivo.escritaArquivo(endereco);
+
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Encerrando a aplicação.");
+        }
 
 
         /*
@@ -34,24 +55,6 @@ public class Principal {
         System.out.println(endereco);
 
         */
-
-        ConsultaCEP consultarCEP = new ConsultaCEP();
-        Scanner leitura = new Scanner(System.in);
-        System.out.println("Informe o CEP a ser recebido: ");
-        System.out.println("Atente-se à formação, o CEP NÃO pode conter mais que oito digitos ou caracteres além de números!");
-        var buscaCEP = leitura.nextLine();
-
-        try {
-            Info endereco = consultarCEP.Consultar(buscaCEP);
-            // System.out.println(endereco);
-            System.out.println("As informações foram registradas em " + endereco.cep() + ".json !");
-            GeradorArquivo gerarArquivo = new GeradorArquivo();
-            gerarArquivo.escritaArquivo(endereco);
-        } catch (RuntimeException | IOException e) {
-            System.out.println(e.getMessage());
-            System.out.println("Encerrando a aplicação.");
-        }
-
 
     }
 }
